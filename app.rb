a528cd5b02a7917735f3e1c47f4c2329c7d98a15
Nathan Steed <steedn@outlook.com>
2020-09-11 23:00:58 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+  get '/name' do
+    # @name = name
+    "My name is Nathan"
+  end
+  get '/hometown' do
+    "My hometown is Raymond"
+  end
+  get '/favorite-song' do
+    # @favorite_song = favorite-song
+    "My favorite song is Hungarian Rhapsody no. 06"
+  end
 end
