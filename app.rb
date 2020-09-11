181d6c559ce9225c3cfa69ab812b086d667dbcf8
Nathan Steed <steedn@outlook.com>
2020-09-11 22:59:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,14 +5,14 @@ class App < Sinatra::Base
     "Hello, World!"
   end
   get '/name' do
-    @name = name
-    "My name is #{@name}"
+    # @name = name
+    "My name is Nathan"
   end
   get '/hometown' do
     "My hometown is Raymond"
   end
   get '/favorite-song' do
-    @favorite_song = favorite-song
-    "My favorite song is #{@favorite_song}"
+    # @favorite_song = favorite-song
+    "My favorite song is Hungarian Rhapsody no. 06"
   end
 end
